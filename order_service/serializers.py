734c2a864cbe26fc0b91e2506f80a6b690c46ca0
Анастасия Пучнина <puchnina.nastya@gmail.com>
2017-12-14 23:21:33 +0300

    Order serializer, viewset added.

---
+++ order_service/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Order
+
+class OrderSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(source='pk', read_only=True)
+    class Meta:
+        model = Order
+        fields = '__all__'
+        read_only_fields('is_paid')
--- order_service/views.py
+++ order_service/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, mixins
+from rest_framework import status
+
+from .serializers import OrderSerializer
+from .models import Order
+
+#TODO: Create tests for views
+
+class OrderViewSet(mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     viewsets.GenericViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
