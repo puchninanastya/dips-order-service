59718ee15994068dc2c21b24d8aa3c7f72280adb
Анастасия Пучнина <puchnina.nastya@gmail.com>
2017-12-14 23:17:58 +0300

    Order model added, admin register.

--- order_service/models.py
+++ order_service/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class Order(models.Model):
+    user = models.IntegerField(
+        blank=False,
+        null=False,
+        verbose_name="By user")
+    order_date = models.DateField(
+        blank=True,
+        null=False,
+        default=now,
+        verbose_name="Order date")
+    amount = models.PositiveIntegerField()
+    is_paid = models.BooleanField(
+        blank=True,
+        null=False,
+        default=False,
+        verbose_name="Has been paid")
+
+    class Meta:
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
+
+'''
+    def __str__(self):
+        return "{}".format(self.pk)
+'''
