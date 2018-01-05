47d47b390dd1fbd9e0c40b730140c5cac674acd1
Анастасия Пучнина <puchnina.nastya@gmail.com>
2018-01-05 22:41:12 +0300

    Add validation to set order payment status with paid query param.

--- order_service/serializers.py
+++ order_service/serializers.py
@@ -29,15 +29,26 @@ class OrderSerializer(serializers.ModelSerializer):
         order_courses = (instance.courses).all()
         order_courses = list(order_courses)
 
-        #retrieve the Order
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
+        # retrieve the Order
+        instance.user = validated_data.get('user', instance.user)
+        instance.order_date = validated_data.get('order_date', instance.order_date)
+        instance.amount = validated_data.get('amount', instance.amount)
+
+        # check payment status to set 'is_paid' field
+        # query string ?paid={num} need to be equal to order amount
+        request = self.context.get("request")
+        is_paid_data = validated_data.pop('is_paid', instance.is_paid)
+        if (is_paid_data != instance.is_paid) and (is_paid_data is True):
+            amount_paid_data = self.context['request'].query_params.get('paid', None)
+            if (amount_paid_data is not None) and (int(amount_paid_data) == instance.amount):
+                instance.is_paid = True
+
         instance.save()
 
+        # retrieve the OrderCourses if exists 'courses'
         if order_courses_data is not None:
             for order_course in order_courses:
                 order_course.delete()
-            #retrieve the OrderCourses
             for order_course_data in order_courses_data:
                 OrderCourse.objects.create(order=instance, **order_course_data)
 
